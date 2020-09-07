10b64eb6566bbbd496699a5ff7cffbdbc145abc1
吕进 <liuding@xiaohongshu.com>
2020-09-08 00:33:59 +0800

    bug fix

--- pkg/apis/one/v1/types.go
+++ pkg/apis/one/v1/types.go
@@ -25,24 +25,30 @@ type Pipeline struct {
 	Spec PipelineSpec `json:"spec"`
 }
 
+// 类型有 Deploy，Restart，Ingress，CI, Scale
 type PipelineSpec struct {
-	Type    PipelineType `json:"type"`
-	Trigger string       `json:"trigger"`
-	Stages  []Stage      `json:"stages"`
+	Trigger         string            `json:"trigger"`
+	Stages          []Stage           `json:"stages"`
+	GlobalResources map[string]string `json:"global_resources"`
+	Type            string            `json:"type"`
 }
 
+// Type: Deploy, DeleteDeploy, Webhook, Start, Wait, Rollback
+// Cluster: 哪个 k8s 集群运行这个 Stage
+// Resources: 这个 stage 关联的 resource，deploy 类型的 stage 才会需要到，但不一定要用到
 type Stage struct {
-	Name    string `json:"name"`
-	Type    string `json:"type"`
-	Cluster string `json:"cluster"`
+	Name     string            `json:"name"`
+	Type     string            `json:"type"`
+	Cluster  string            `json:"cluster"`
+	Reources map[string]string `json:"resources"`
 }
 
-type PipelineType string
-
-var (
-	GlobalPipeline   PipelineType = "Global"
-	CustomerPipeline PipelineType = "Customer"
-)
+//type PipelineType string
+//
+//var (
+//	GlobalPipeline   PipelineType = "Global"
+//	CustomerPipeline PipelineType = "Customer"
+//)
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type PipelineList struct {
